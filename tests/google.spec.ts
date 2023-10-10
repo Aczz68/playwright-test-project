a0235f258a05917c4315f5183554f0c66b08373f
lvyize <yzz31035@163.com>
2023-10-10 17:28:21 +0800

    playWright 配置

--- tests/google.spec.ts
+++ tests/google.spec.ts
@@ -1,3 +1,13 @@
-import { test, example } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test("");
+test("Page demo", async ({ page }) => {
+  // type="search"
+  await page.goto("https://www.google.com");
+  // 定位器:标题属性为搜索的
+  const input = page.locator("[type=search]");
+  // 输入内容 input.type('playwright')
+  await input.fill("playwright");
+  // 按下按键 input.press('Enter')
+  await input.press("Enter");
+  await expect(page).toHaveTitle("playwright - Google 搜尋");
+});
